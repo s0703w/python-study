b134e8a195348b44e17b5f6d11a13fd5b2cff7c7
s0703w <64122008+s0703w@users.noreply.github.com>
2020-05-06 14:01:04 +0900

    과제제

--- 과제 1-1.py
+++ 과제 1-1.py
@@ -1,1 +1,10 @@
-n = int(input("N: "))
+x = int(input("X: "))
+y = int(input("Y: "))
+z = int(input("Z: "))
+
+if x > y > z:
+    print(y)
+if z > y > z:
+    print(y)
+elif y > z > x:
+    print(z)
